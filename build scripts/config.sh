8ac9de6c0538b4b2a923e5076201e24bff5f75c7
operation404 <operation404@gmail.com>
2024-06-15 20:16:10 -0700

    todo

--- build scripts/config.sh
+++ build scripts/config.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
 git_commit=a4f2ea91a1bd18f70a43ff4c1377db49b56bc3f0
+
+# TODO
+# have the docker compose file set up an environment variable for this,
+# so that it's all controlled in one place
 build_dir=/build
