0cbfc821425e5d5ba6db96fc14fcddfd0e9b3f2c
operation404 <operation404@gmail.com>
2024-07-03 10:40:53 -0700

    updated build scripts

--- build scripts/build.sh
+++ build scripts/build.sh
@@ -8,12 +8,10 @@ cd ./godot
 
 git checkout $git_commit
 
-scons platform=windows target=editor arch=x86_64 d3d12=yes \
+scons platform=windows arch=x86_64 d3d12=yes \
     dxc_path=${build_dir}/dxc mesa_libs=${build_dir}/mesa pix_path=${build_dir}/pix \
+    ${build_options} \
     module_mono_enabled=yes
-    # Options for build modes:
-    # dev_mode=yes \
-    # production=yes \
     
 scons platform=windows vsproj=yes
 
--- build scripts/config.sh
+++ build scripts/config.sh
@@ -6,3 +6,11 @@ git_commit=a4f2ea91a1bd18f70a43ff4c1377db49b56bc3f0
 # have the docker compose file set up an environment variable for this,
 # so that it's all controlled in one place
 build_dir=/build
+
+build_options="     \  
+target=editor       \  
+dev_mode=yes        \  
+debug_symbols=yes   \  
+dev_build=yes       \  
+optimize=debug      \ 
+"
