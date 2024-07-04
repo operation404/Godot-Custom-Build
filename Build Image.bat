eb341e8654628999f759513a41f253b63538387e
operation404 <operation404@gmail.com>
2024-07-04 16:35:26 -0700

    bat files for building image and running container

---
+++ Build Image.bat
@@ -0,0 +1,2 @@
+cd D:\Projects and Repositories\Godot Building\custom-build
+docker compose build
---
+++ Run Container.bat
@@ -0,0 +1,4 @@
+cd D:\Projects and Repositories\Godot Building\custom-build
+docker compose run builder
+:: Keep the command prompt open and interactive
+cmd /k
