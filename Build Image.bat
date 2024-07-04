ba0db8d0efa67f5b44d631d74c66837ef94ec5b1
operation404 <operation404@gmail.com>
2024-07-04 16:58:56 -0700

    fix incorrect arg

--- Build Image.bat
+++ Build Image.bat
@@ -1,4 +1,4 @@
 cd D:\Projects and Repositories\Godot Building\custom-build
 docker compose build
 :: Remove previous and now outdated versions of the image
-docker image prune -y
+docker image prune -f
