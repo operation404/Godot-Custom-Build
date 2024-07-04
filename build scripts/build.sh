adfb7d47219e040453398e38c4dc7aefd3d6d6b7
operation404 <operation404@gmail.com>
2024-07-04 15:59:11 -0700

    build vs solution with debug symbols

--- build scripts/build.sh
+++ build scripts/build.sh
@@ -11,6 +11,6 @@ git checkout $git_commit
 scons dxc_path=${build_dir}/dxc mesa_libs=${build_dir}/mesa pix_path=${build_dir}/pix \
     ${build_options}
     
-scons platform=windows vsproj=yes
+scons platform=windows vsproj=yes dev_build=yes
 
 cd ..
