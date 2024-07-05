1dcaf8bf216163780eda2b8432da152fbd7bd24c
operation404 <operation404@gmail.com>
2024-07-04 23:21:26 -0700

    formatting

--- build scripts/clean.sh
+++ build scripts/clean.sh
@@ -6,7 +6,10 @@ source config.sh
 
 cd ./godot
 
-scons --clean dxc_path=${BUILD_DIR}/dxc mesa_libs=${BUILD_DIR}/mesa pix_path=${BUILD_DIR}/pix \
+pyston-scons --clean                \
+    dxc_path=${BUILD_DIR}/dxc       \
+    mesa_libs=${BUILD_DIR}/mesa     \
+    pix_path=${BUILD_DIR}/pix       \
     ${build_options}
 
 cd ..
