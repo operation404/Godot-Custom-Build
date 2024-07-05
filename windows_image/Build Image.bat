551f41575a007b87f270c347e95cc9a8d141cba7
operation404 <operation404@gmail.com>
2024-07-05 14:12:37 -0700

    working on docker container of a windows server os for building godot using vs build tools

---
+++ windows_image/Build Image.bat
@@ -0,0 +1,6 @@
+cd D:\Projects and Repositories\Godot Building\custom-build\windows_image
+docker compose build
+:: Remove previous and now outdated versions of the image
+docker image prune -f
+:: Keep the command prompt open and interactive
+ cmd /k
---
+++ windows_image/Run Container.bat
@@ -0,0 +1,5 @@
+cd D:\Projects and Repositories\Godot Building\custom-build\windows_image
+:: --rm removes the container after running, these are one and done containers
+docker compose run --rm winbuilder
+:: Keep the command prompt open and interactive
+:: cmd /k
