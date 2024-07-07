fb879de8421532df6f0db6a82bb51757ecc1b732
operation404 <operation404@gmail.com>
2024-07-06 23:08:57 -0700

    Remove spaces after escape char

--- windows_image/build scripts/build.bat
+++ windows_image/build scripts/build.bat
@@ -7,10 +7,10 @@ cd %BUILD_DIR%\godot
 
 git checkout %GIT_COMMIT%
 
-scons                               ^
-    dxc_path=%BUILD_DIR%\dxc        ^ 
-    mesa_libs=%BUILD_DIR%\mesa      ^
-    pix_path=%BUILD_DIR%\pix        ^
+scons                                   ^
+    dxc_path=c:%BUILD_DIR%\dxc          ^
+    mesa_libs=c:%BUILD_DIR%\mesa        ^
+    pix_path=c:%BUILD_DIR%\pix          ^
     %BUILD_OPTS%
 
 cd %BUILD_DIR%\scripts
