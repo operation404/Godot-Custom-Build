b6f49a9d8dcbbc3baad7a39cf95accf3b048bef3
operation404 <operation404@gmail.com>
2024-07-06 21:57:00 -0700

    install 7zip and git

--- windows_image/Build Image.bat
+++ windows_image/Build Image.bat
@@ -1,6 +1,7 @@
 cd D:\Projects and Repositories\Godot Building\custom-build\windows_image
 docker compose build
 :: Remove previous and now outdated versions of the image
+docker container prune -f
 docker image prune -f
 :: Keep the command prompt open and interactive
  cmd /k
