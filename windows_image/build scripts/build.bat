2b41664ead755fdbef0c3f059f5200f9f9c21787
operation404 <operation404@gmail.com>
2024-07-06 22:53:52 -0700

    Finish base build script. Disable git safe directory warnings

--- windows_image/build scripts/build.bat
+++ windows_image/build scripts/build.bat
@@ -1,4 +1,16 @@
-cd %BUILD_DIR%
+cd %BUILD_DIR%\scripts
+
+:: Set up build environment variables
 call config.bat
+
 cd %BUILD_DIR%\godot
 
+git checkout %GIT_COMMIT%
+
+scons                               ^
+    dxc_path=%BUILD_DIR%\dxc        ^ 
+    mesa_libs=%BUILD_DIR%\mesa      ^
+    pix_path=%BUILD_DIR%\pix        ^
+    %BUILD_OPTS%
+
+cd %BUILD_DIR%\scripts
