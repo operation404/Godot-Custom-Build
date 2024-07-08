600709d4106320cc3890ae5ee1895026f5bfa08c
operation404 <operation404@gmail.com>
2024-07-07 21:41:27 -0700

    Change container tags to be unique

--- windows_minimum_test/Run Container.bat
+++ windows_minimum_test/Run Container.bat
@@ -1,4 +1,4 @@
 :: --rm removes the container after running, these are one and done containers
-docker compose run --rm winbuilder
+docker compose run --rm minbuilder
 :: Keep the command prompt open and interactive
 :: cmd /k
